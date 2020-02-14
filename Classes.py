057dfc9b9251764f5f72081d2a0b94732f3d0a3c
Vincent Wren <wrenvin@gmail.com>
2020-02-14 13:25:08 -0600

    Updated Classes

--- Classes.py
+++ Classes.py
@@ -1,1 +1,19 @@
 
+class Employee:
+    #Use self for functions all the time
+    def __init__(self, first, last, wage):#Constructor, initalizes values
+        self.first = first
+        self.last = last
+        self.wage = wage
+        self.email = first + '.' + last + '@gmail.com'
+    
+    def fullname(self): #This is a method
+        return '{} {}'.format(self.first, self.last)
+    
+
+emp_1 = Employee('Vincent', 'Wren', '1000')
+emp_2 = Employee('Carlos', 'Garica', '5000')#Each unique instance of class, dont pass self
+
+Employee.fullname(emp_1) # similar to below, but passes instance through generic class, dont use
+print(emp_2.fullname())
+
